a0cb77deddd7906a2fe8c3caac0e1dfca283ffd9
Michael Dera <michaeldera@live.com>
2020-09-16 15:52:21 +0200

    git init

---
+++ src/views/pages/error500.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Error500 = () => {
+  return <div>Error 500</div>
+}
---
+++ src/views/pages/index.ts
@@ -0,0 +1,4 @@
+export * from "./error404";
+export * from "./error500";
+export * from "./login";
+export * from "./register";
