369e1d440ba2e6aa0926a4cf489a33e35b958a58
Michael Dera <michaeldera@live.com>
2020-09-16 16:00:44 +0200

    bug fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app';
+import App from './apps';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
